000430135092cb99511b61e39ac8c574b23c85b5
dato7898 <davidd7598@gmail.com>
2022-08-06 22:14:25 +0600

    update

--- public/Assests/js/app.js
+++ public/Assests/js/app.js
@@ -171,10 +171,11 @@ const AppProcess = (function () {
     let iceConfiguration = {
         iceServers: [
             {
-                urls: "stun:stun.l.google.com:19302"
-            },
-            {
-                urls: "stun:stun1.l.google.com:19302"
+                "urls": [
+                    "turn:13.250.13.83:3478?transport=udp"
+                ],
+                "username": "YzYNCouZM1mhqhmseWk6",
+                "credential": "YzYNCouZM1mhqhmseWk6"
             }
         ]
     }
